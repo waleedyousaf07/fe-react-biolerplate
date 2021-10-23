94c467828bef7672baefd1bcc9b27a39e6b54420
waleedyousaf07 <waleedyousaf07@gmail.com>
2021-10-23 15:52:42 +0500

    adds and configures husky and adds test and lint precommit hooks

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,6 +1,11 @@
-const reportWebVitals = onPerfEntry => {
+/* eslint-disable no-undef */
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import('web-vitals').then(({
+      getCLS,
+      getFID,
+      getFCP,
+    }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
