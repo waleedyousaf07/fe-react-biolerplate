d09e6089742b07539b62def8518bacbf307494eb
waleedyousaf07 <waleedyousaf07@gmail.com>
2021-10-23 14:11:38 +0500

    removes unwanted code and adds colors and spacing scss

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders h1 tag with text Heyy!', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingText = screen.getByText(/Heyy!/i);
+  expect(headingText).toBeInTheDocument();
 });
